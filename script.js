3a3687c30d6235e7e61fba879e4533d06e3990a7
VicNW <97852276+VicNW@users.noreply.github.com>
2022-06-24 11:51:13 +0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+let myName = prompt("Enter your name")
+let myHeight = prompt("Enter your Height")
+let myCountry = prompt("Enter your country")
+
+alert("Name : " + myName + "\n" +
+      " Age : " + myHeight + "\n" + 
+  "Country : " + myCountry);
+
+document.write("Name : " + myName + "<br>");
+document.write("Age : " + myHeight + "<br>");
+document.write("Country : " + myCountry);
